6dfa4ee6245fbe7c58f06f1da4f452d63156c778
brian_schwartz <bjs9yv@virginia.edu>
2015-10-20 16:37:02 -0400

    added more prints

--- onion.py
+++ onion.py
@@ -3,18 +3,15 @@ from Crypto.PublicKey import RSA
 
 class NetworkDevice:
     def __init__(self):
-        seed = Random.new().read
-        self.key_pair = RSA.generate(2048, seed)
+        rand_func = Random.new().read
+        self.key_pair = RSA.generate(2048, rand_func)
         self.public_key = self.key_pair.publickey()
 
     def exchange_public_key(self):
         return self.public_key
     
     def decrypt_string(self, ciphertext):
-        if type(ciphertext) is bytes:
-            cleartext = self.key_pair.decrypt(ciphertext)
-        else:
-            cleartext = self.key_pair.decrypt(ciphertext).decode(encoding='UTF-8')
+        cleartext = self.key_pair.decrypt(ciphertext)
         return cleartext
 
     def encrypt_string(self, string, public_key):
@@ -29,8 +26,9 @@ if __name__ == "__main__":
     # Alice wants to learn some cool hacks from a website
     alice = NetworkDevice()
     dest_ip_addr = '1.3.3.7'
+    print('where alice wants to go: ', dest_ip_addr, '\n')
 
-    # she decides to use Tor, and chooses an entry node
+    # she decides to use Tor, and chooses an entry node from the Tor Directory
     entry_node = NetworkDevice()
     
     # she gets in contact with the entry node and exchanges public keys
@@ -58,11 +56,14 @@ if __name__ == "__main__":
     # the entry node recieves the message and decrypts it to discover its contents...
     decrypted1 = entry_node.decrypt_string(message_to_entry_node)
     # this message is still encrypted and will only reveal the ip of the next hop
-    print('decrypted message: ', decrypted1)
+    print('what entry node sees: ', decrypted1, '\n')
     # the entry node sends the message to the next hop, who decrypts and sends to the next
     decrypted2 = tor_node1.decrypt_string(decrypted1)
+    print('what node1 sees: ', decrypted2, '\n')
     # removing each layer like the layers of an onion... omg the name makes sense!
     decrypted3 = tor_node2.decrypt_string(decrypted2)
+    print('what node2 sees: ', decrypted3, '\n')
     # the exit node peels off the last layer of the onion
+    # it also performs a checksum calculation to make sure the data has not been tampered with 
     decrypted_message = exit_node.decrypt_string(decrypted3)
-    print('decrypted message: ', decrypted_message)
+    print('what exit node sees: ', decrypted_message.decode(encoding='UTF-8'))
